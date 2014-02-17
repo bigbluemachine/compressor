c718af52b1f3a33046b8dbc0aba07347a323342c
notacompany-org <notacompany-org@users.noreply.github.com>
2014-02-17 08:15:54 -0800

    Update PrefixFreeCode.java

--- core/PrefixFreeCode.java
+++ core/PrefixFreeCode.java
@@ -1,74 +1,78 @@
 package core;
 
-import java.util.Arrays;
-
 public class PrefixFreeCode {
+	static final long INF = 1L << 48;
+	static long[][] CURR = new long[129][256];
+	static long[][] PREV = new long[129][256];
+	static int[][][] BEST = new int[129][256][256];
 	static int[] F;
 	static int L;
-	static long[][][] T = new long[128][256][256];
-	static int[][][] B = new int[128][256][256];
 
-	static long M(int R, int N, int D) {
-		if (T[N - 1][R][D - 1] >= 0) {
-			return T[N - 1][R][D - 1];
+	static void initDp(long[][] T) {
+		for (int i = 0; i <= 128; i++) {
+			T[i][0] = 0;
+			for (int j = 1; j < 256; j++) {
+				T[i][j] = INF;
+			}
 		}
+	}
 
-		long best = Long.MAX_VALUE;
+	static void dp(int N, int R, int D) {
+		long best = INF;
 		long diff = 0;
 
 		int maxI = Math.min(R, 2 * N);
 		for (int i = 1; i <= maxI; i++) {
 			diff += F[L - R - i] + (D + 1) * F[L - R - 1 + i];
-			long test = diff + M(R - i, i, D + 1);
+			long test = diff + PREV[i][R - i];
 			if (test < best) {
 				best = test;
-				B[N - 1][R][D - 1] = i;
+				BEST[N][R][D] = i;
 			}
 		}
 
-		return T[N - 1][R][D - 1] = best;
+		CURR[N][R] = best;
 	}
 
-	public static int[] depths(int[] freq) {
-		F = freq;
+	public static int[] getDepths(int[] freqs) {
+		F = freqs;
 		L = F.length;
 
-		for (long[][] U : T) {
-			for (long[] V : U) {
-				Arrays.fill(V, -1);
+		for (int D = 255; D >= 1; D--) {
+			long[][] temp = CURR;
+			CURR = PREV;
+			PREV = temp;
+			initDp(CURR);
+
+			for (int N = 1; N < 128; N++) {
+				for (int R = 1; 2 * N + R <= L; R++) {
+					dp(N, R, D);
+				}
 			}
 		}
-
-		for (int i = 0; i < 128; i++) {
-			for (int j = 0; j < 256; j++) {
-				T[i][0][j] = 0;
-			}
-		}
-
-		int R = L - 2, N = 1, D = 1;
-		M(R, N, D);
 
 		int[] G = new int[L];
 		G[0] = G[1] = 1;
-		int lastIndex = 1;
-
-		for (;;) {
-			int i = B[N - 1][R][D - 1];
-			if (i == 0) {
-				break;
+		for (int D = 1, N = 1, R = L - 2; (N = BEST[N][R][D]) > 0; D++) {
+			for (int i = 1; i <= N; i++) {
+				G[L - R - i] = G[L - R - 1 + i] = D + 1;
 			}
-
-			for (int j = 0; j < i; j++) {
-				G[lastIndex - j] = D + 1;
-				G[lastIndex + j + 1] = D + 1;
-			}
-
-			lastIndex += i;
-			R -= i;
-			N = i;
-			D++;
+			R -= N;
 		}
 
 		return G;
+	}
+
+	public static int[] getDepths(int[][] symbolList, int symbolCount) {
+		if (symbolCount == 1) {
+			return new int[] { 1 };
+		}
+
+		int[] freqs = new int[symbolCount];
+		for (int i = 0; i < symbolCount; i++) {
+			freqs[i] = symbolList[i][1];
+		}
+
+		return getDepths(freqs);
 	}
 }
